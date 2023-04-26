3024c08a385e66c53279dc53ce375a93279c2d6b
zhouflora <108855722+zhouflora@users.noreply.github.com>
2023-04-26 06:51:23 -0700

    portfolio update

---
+++ portfolio/scripts/photography.js
@@ -0,0 +1,29 @@
+// Menu Show Hidden
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+// Show Menu
+// Validate if constant exists
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// Hide Menu
+// Validate if constant exists
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+// Remove Menu Mobile
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
